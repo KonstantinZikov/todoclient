c41b472b932b027d7bcc3bf0634fac767723c3f4
KonstantinZikov <konstantin.zikov.s@gmail.com>
2016-09-13 23:50:09 +0300

    some_refactoring_and_design_fixes

---
+++ Scripts/ajax-manager.js
@@ -0,0 +1,46 @@
+﻿function AjaxManager() {
+    // Send post request with creating entity to server and calls callback if success.
+    // @entity: todo entity for creating.
+    // @callback: function that calls if success. Should have one param - id of created entity.
+    this.create = function (entity, callbalck) {
+        $.ajax({
+            url: "/api/todos",
+            type: "POST",
+            contentType: 'application/json',
+            data: JSON.stringify(entity),
+            success: callbalck
+        });
+    }
+
+    // Send put request with updating entity to server.
+    // @entity: todo entity for updating.
+    this.update = function (entity) {
+        $.ajax({
+            url: "/api/todos",
+            type: "PUT",
+            contentType: 'application/json',
+            data: JSON.stringify(entity)
+        });
+    }
+
+    // Gets all values from server and calls callback if success.
+    // @callback: function that calls if success and
+    // should have one param - collection of 'todo' entities.
+    this.get = function (callback) {
+        $.ajax({
+            url: "/api/todos",
+            type: "GET",
+            contentType: 'application/json',
+            success: callback
+        });
+    }
+
+    // Deletes entity with selected id from server.
+    // @id: id of deleting entity.
+    this.delete = function (id) {
+        $.ajax({
+            url: "/api/todos/" + id,
+            type: "DELETE"
+        });
+    }
+}
--- Scripts/common.js
+++ Scripts/common.js
@@ -1,4 +1,102 @@
-$(function () {
+var server = new AjaxManager();
+
+// loads all values from server
+function load() {
+    server.get(function (todos) {
+        todos.forEach(function (item, index, array) {
+            $newLine = $(".todo-block .template-value").clone();
+            $newLine.removeClass("template-value");
+            $newLine.find(".todo-name").text(item.Name);
+            $newLine.attr("data-id", item.ToDoId);
+            $newLine.find("input:checkbox").prop("checked", item.IsCompleted);
+            $(".todo-block").prepend($newLine);
+            bindActions($newLine);
+        });
+    })
+}
+
+// edit-/save-buttin action
+function editClick() {
+    $this = $(this);
+    $line = $this.parent().parent();
+    var status = $line.attr("data-edit");
+
+    if (status == "true")
+        endEdit($line, $this);
+    else
+        startEdit($line, $this);
+}
+
+// Called, when user want to start edit.
+// @$line: line with todo-value
+// @$button: edit-button
+function startEdit($line, $button) {
+    $line.attr("data-edit", "true");
+    var $field = $line.find(".todo-name");
+    var name = $field.text();
+    var $input = $("<input>");
+    $input.val(name);
+    $input.addClass("todo-input");
+    $field.after($input);
+    $field.remove();
+    $button.text("Save");
+}
+
+// Called, when user finished to edit and want to save value.
+// Causes updating on server.
+// @$line: line with input
+// @$button: save-button
+function endEdit($line, $button) {
+    $line.attr("data-edit", "false");
+    var $input = $line.find(".todo-input");
+    var name = $input.val();
+    var $field = $("<div>");
+    $field.text(name);
+    $field.addClass("todo-name");
+    $input.after($field);
+    $input.remove();
+    $button.text("Edit");
+    var id = +$line.attr("data-id");
+    var isCompleted = $line.find(".checkbox").prop("checked");
+
+    var entity = {
+        ToDoId: id,
+        IsCompleted: isCompleted,
+        Name: name
+    }
+
+    if (id == -1) {
+        server.create(entity, function (index) {
+            $line.attr("data-id", index);
+        });
+    }
+    else {
+        server.update(entity);
+    }
+}
+
+// Delete todo action
+function deleteClick() {
+    $line = $(this).parent().parent();
+    var id = $line.attr("data-id");
+    if (id != -1) {
+        server.delete(id);
+    }
+    
+    $line.hide(300, function () {
+        $line.remove();
+    });
+}
+
+// Search delete- and edit-buttons in element (it should be .todo-value element)
+// and binds actions to them
+function bindActions(element) {
+    element.find(".delete-button").on("click", deleteClick);
+    element.find(".edit-button").on("click", editClick);
+}
+
+// Binds add-new-todo logic to Add button
+function bindAddAction() {
     $(".add-button").on("click", function () {
         $newLine = $(".todo-block .template").clone();
         $newLine.removeClass("template");
@@ -7,101 +105,10 @@ $(function () {
         $newLine.show(300);
         bindActions($newLine);
     })
+}
 
-    function load() {
-        $.ajax(
-        {
-            url: "/api/todos",
-            type: "GET",
-            contentType: 'application/json',
-            success: function (todos) {
-                todos.forEach(function (item, index, array) {
-                    $newLine = $(".todo-block .template-value").clone();
-                    $newLine.removeClass("template-value");
-                    $newLine.find(".todo-name").text(item.Name);
-                    $newLine.attr("data-id", item.ToDoId);
-                    $newLine.find("input:checkbox").prop("checked", item.IsCompleted);                  
-                    $(".todo-block").prepend($newLine);
-                    bindActions($newLine);
-                });
-            }
-        });
-    }
-
+$(function () {
     load();
-
-    function editClick() {
-        $line = $(this).parent().parent();
-        var status = $line.attr("data-edit");
-        if (status == "true") {
-            $line.attr("data-edit", "false");
-            var $input = $line.find(".todo-input");
-            var name = $input.val();
-            var $field = $("<div>");
-            $field.text(name);
-            $field.addClass("todo-name");
-            $input.after($field);
-            $input.remove();
-            $(this).text("Edit");
-
-            var id = +$line.attr("data-id");
-            var isCompleted = $line.find(".checkbox").prop("checked");
-            if (id == -1) {
-                var method = "POST";
-            }
-            else {
-                method = "PUT"
-            }
-
-            $.ajax(
-            {
-                url: "/api/todos",
-                type: method,
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    ToDoId: id,
-                    IsCompleted: isCompleted,
-                    Name: name
-                }),
-                success: function (index) {
-                    if (method == "POST") {
-                        $line.attr("data-id", index);
-                    }
-                }
-            });
-        }
-        else {
-            $line.attr("data-edit", "true");
-            var $field = $line.find(".todo-name");
-            var name = $field.text();
-            var $input = $("<input>");
-            $input.val(name);
-            $input.addClass("todo-input");
-            $field.after($input);
-            $field.remove();
-
-            $(this).text("Save");
-        }
-        
-    }
-
-    function deleteClick() {
-        $line = $(this).parent().parent();
-        var id = $line.attr("data-id");
-        $.ajax({
-            url: "/api/todos/" + id,
-            type: "DELETE"
-        });
-
-        $line.hide(300, function () {
-            $line.remove();
-        });
-    }
-
-    function bindActions(element){
-        element.find(".delete-button").on("click", deleteClick);
-        element.find(".edit-button").on("click", editClick);
-    }
-
+    bindAddAction();
     bindActions($(".todo-value"));
 })
